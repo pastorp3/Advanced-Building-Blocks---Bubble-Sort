64e2027bbe01fd953d94c025e22cd8176f0bc2e7
Jose <pastorpedraza3@gmail.com>
2020-08-12 10:37:52 -0500

    add buble_sort_by

--- main.rb
+++ main.rb
@@ -7,8 +7,27 @@ def bubble_sort (array_to_sort)
             array_to_sort[i], array_to_sort[i.next] =array_to_sort[i.next], array_to_sort[i]
             sorted = false
         end
+    end
+    end
+    array_to_sort
+ end
+
+ def buble_sort_by(array)
+    n = array.length
+    loop do 
+        switch = false 
+        (n-1).times do |e|
+            if (yield array[e], array[e+1]).positive?
+                array[e], array[e+1] = array[e+1], array[e]
+                switch = true 
+            end 
         end
-        end
-        array_to_sort
-        end
-        p bubble_sort([8,5,7,2,3,6,4,200,84,1,0,6,2,7])
+    break if switch == false 
+    end 
+    array
+
+end
+
+
+p bubble_sort([8,5,7,2,3,6,4,200,84,1,0,6,2,7])
+
