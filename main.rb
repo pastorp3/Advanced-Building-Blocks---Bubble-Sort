2518612395804c55312a0b37f8464adeb68205d1
Jose <pastorpedraza3@gmail.com>
2020-08-12 11:19:02 -0500

    fix linters

--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
-def bubble_sort (array_to_sort)
+def bubble_sort(array_to_sort)
     sorted = false
     until sorted
-        sorted = true
+    sorted = true
     (0... array_to_sort.length - 1).each do |i|
         if array_to_sort[i] > array_to_sort[i.next]
             array_to_sort[i], array_to_sort[i.next] =array_to_sort[i.next], array_to_sort[i]
@@ -31,6 +31,6 @@ bsort_by = buble_sort_by(["hello","hey","hi","her"]) do |left,right|
     left.length - right.length
 end
 
-p bubble_sort([8,5,7,2,3,6,4,200,84,1,0,6,2,7])
+p bubble_sort([8 ,5 ,7 ,2 , 3, 6, 4, 200, 84, 1, 0, 6, 2, 7])
 puts bsort_by
 
